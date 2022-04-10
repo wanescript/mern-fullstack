cb379149eec7dedf53a57823096779d0b2e73f23
handmakerzhm <handmakerzhm@gmail.com>
2022-04-09 17:14:01 -0700

    second day commit

--- backend/controllers/goalsController.js
+++ backend/controllers/goalsController.js
@@ -5,7 +5,7 @@
 
 const getGoals = (req,res)=>{
     res.status(200).json({msg:'get goals'})
-    console.log('get goals coming from the controller folder')
+    console.log('get goals function is coming from the controller folder')
 }
 //@description Set goal
 //@route  POST/api/goals
@@ -13,7 +13,7 @@ const getGoals = (req,res)=>{
 
 const setGoals = (req,res)=>{
     res.status(200).json({msg:'create goal'})
-    console.log('create goal')
+    console.log('create goals function is coming from the controller folder')
 }
 //@description Update goals
 //@route  Update/api/goals
@@ -29,7 +29,7 @@ const updateGoals = (req,res)=>{
 
 const deleteGoals = (req,res)=>{
     res.status(200).json({msg:`Delete goal ${req.params.id}`})
-    console.log('delete goals are coming from the controller folder')
+    console.log('delete goals function is coming from the controller folder')
 }
 
 
