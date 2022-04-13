d16119e2a0320143de3731415527f53dd127f150
handmakerzhm <handmakerzhm@gmail.com>
2022-04-12 17:42:14 -0700

    tuesday commit

--- backend/controllers/goalsController.js
+++ backend/controllers/goalsController.js
@@ -1,4 +1,5 @@
-
+//wrapping each function with the async handler package
+const asyncHandler = require('express-async-handler')
 /*
 CRUD functionality in the controller folder, which is located inside the backend folder. */
 
@@ -7,34 +8,40 @@ CRUD functionality in the controller folder, which is located inside the backend
 //@route  GET/api/goals
 //@access Private
 
-const getGoals = (req,res)=>{
+const getGoals = asyncHandler (async(req,res)=>{
     res.status(200).json({msg:'get goals'})
     console.log('get goals function is coming from the controller folder')
-}
+})
 //@description Set/CREATE goal
 //@route  POST/api/goals
 //@access Private
 
-const setGoals = (req,res)=>{
-    res.status(200).json({msg:'create goal'})
+const setGoals = asyncHandler (async(req,res)=>{
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('please enter the correct key name')
+    }else {
+    res.status(200).json({msg:'Goal created'})
     console.log('create goals function is coming from the controller folder')
-}
+    // console.log(req.body)
+    }
+})
 //@description Update goals
 //@route  PUT /api/goals/:id
 //@access Private
 
-const updateGoals = (req,res)=>{
+const updateGoals = asyncHandler (async(req,res)=>{
     res.status(200).json({msg:`Update goal ${req.params.id}`})
     console.log(`update goals function is coming from the controller folder.${req.params.id}`)
-}
+})
 //@description Delete goals
 //@route  DELETE/api/goals/:id
 //@access Private
 
-const deleteGoals = (req,res)=>{
+const deleteGoals = asyncHandler (async(req,res)=>{
     res.status(200).json({msg:`Delete goal ${req.params.id}`})
     console.log(`delete goals function is coming from the controller folder.${req.params.id}`)
-}
+})
 
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,12 @@ const app = express()
 const PORT = process.env.PORT ||5000
 const dotenv = require('dotenv').config()
 const {getGoals,setGoals,updateGoals,deleteGoals}= require('./controllers/goalsController')
+const {errorHandler} = require('./middleware/errorMiddleware')
 
+//two lines below enables you to read form input
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 app.use('/api/goals', require('./routes/goals'))
+app.use(errorHandler)
 
 app.listen(PORT,()=>{`Server is up and running on localhost://${PORT}`})
