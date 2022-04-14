186c97926ecf9c4ced220dbbaac3f324654af5fa
handmakerzhm <handmakerzhm@gmail.com>
2022-04-13 18:38:27 -0700

    next day on backend api from yesterday

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -38,7 +38,9 @@ const registerUser = asyncHandler (async(req,res)=>{
         res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
+
         })
     }else{
         res.status(400)
@@ -60,6 +62,7 @@ const loginUser = asyncHandler(async(req,res)=>{
         _id: user.id,
         name: user.name,
         email: user.email,
+        token: generateToken(user._id)
     })
 }else {
     res.status(400)
@@ -68,12 +71,25 @@ const loginUser = asyncHandler(async(req,res)=>{
 
 //@desc Get user data
 //@route GET /api/users/me
-//@access Public
+//@access Private
 
 
 const getMe = asyncHandler(async(req,res)=>{
-    res.json({msg:'Display user data'})
+    const {_id,name,email} = await User.findById(req.user.id)
+    res.status(200).json({
+        id: _id,
+        name,
+        email
+    })
+    
 })
+
+//Generate JWT token
+const generateToken = (id)=>{
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn:'30d',
+    })
+}
 
 module.exports = {
     registerUser,
